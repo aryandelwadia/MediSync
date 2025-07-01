d36d2531c7b2252692f89051face23abb2f69652
Aryan Delawadia <aryandelwadia@gmail.com>
2025-07-01 17:51:22 +0530

    commit

---
+++ backend/createAdmin.js
@@ -0,0 +1,31 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+const Admin = require('./models/Admin');
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log('Connected to MongoDB'))
+.catch((err) => console.error('Could not connect to MongoDB', err));
+
+async function createAdmin() {
+  const admin = new Admin({
+    firstName: "admin",
+    lastName: "admin",
+    email: "admin@gmail.com",
+    password: "admin", 
+    role: "admin"
+  });
+
+  try {
+    await admin.save();
+    console.log('Admin created successfully');
+  } catch (error) {
+    console.error('Error creating admin:', error);
+  } finally {
+    mongoose.connection.close();
+  }
+}
+
+createAdmin();
